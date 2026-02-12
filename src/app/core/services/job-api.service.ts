1adf257938cd2fc0b0c9b6db1bca8b9dae16cc66
Hichamelhmaydi <ct1594065@gmail.com>
2026-02-12 14:09:02 +0100

    add fonction for get job with id an gonction of search

--- src/app/core/services/job-api.service.ts
+++ src/app/core/services/job-api.service.ts
@@ -1,40 +1,30 @@
-import { HttpClient, HttpParams } from "@angular/common/http";
-import { JobApiResponse } from "../models/job.model";
-import { inject, Injectable } from "@angular/core";
-import { environment } from "../../../environments/environment.development";
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { Job, JobApiResponse, JobFilters } from '../models/job.model';
+import { environment } from '../../../environments/environment.development';
 
-export interface JobFilters {
-  page?: number;
-  location?: string;
-  level?: string;
-  category?: string;
-}
-
-@Injectable({
-  providedIn: 'root'
-})
+@Injectable({ providedIn: 'root' })
 export class JobService {
+  private http = inject(HttpClient);
+  private readonly apiUrl = environment.jobApi; 
 
-    private http = inject(HttpClient);
-    private readonly apiUrl = environment.jobApi;
+  getJobs(filters: JobFilters = {}) {
+    let params = new HttpParams()
+      .set('page', (filters.page ?? 1).toString())
+      .set('descending', 'true');
 
-    getJobs(filters: JobFilters = {}) {
-        let params = new HttpParams()
-            .set('page', (filters.page ?? 1).toString())
-            .set('descending', 'true');
+    if (filters.location) params = params.set('location', filters.location);
+    if (filters.level) params = params.set('level', filters.level);
+    if (filters.category) params = params.set('category', filters.category);
 
-        if (filters.location) {
-            params = params.set('location', filters.location);
-        }
+    return this.http.get<JobApiResponse>(`${this.apiUrl}/jobs`, { params });
+  }
 
-        if (filters.level) {
-            params = params.set('level', filters.level);
-        }
+  getJobById(id: number) {
+    return this.http.get<Job>(`${this.apiUrl}/jobs/${id}`);
+  }
 
-        if (filters.category) {
-            params = params.set('category', filters.category);
-        }
-
-        return this.http.get<JobApiResponse>(this.apiUrl, { params });
-    }
+  searchJobs(filters: JobFilters = {}) {
+    return this.getJobs(filters);
+  }
 }
