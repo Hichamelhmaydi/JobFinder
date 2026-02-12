670df5dcf24b97475e21f820a658965fb45f18e7
Hichamelhmaydi <ct1594065@gmail.com>
2026-02-12 10:05:53 +0100

    add job model v2

--- src/app/core/models/job.model.ts
+++ src/app/core/models/job.model.ts
@@ -1,12 +1,25 @@
-export interface job {
+export interface Job {
   id: number;
   name: string;
   type: string;
-  publication_date: string;  
-  contents: string;          
+  publication_date: string;
+  contents: string;
   locations: { name: string }[];
   categories: { name: string; short_name: string }[];
   levels: { name: string }[];
-  refs: { landing_page: string };  
-  company: { name: string; id: number };
+  refs: {
+    landing_page: string;
+  };
+  company: {
+    name: string;
+    id: number;
+  };
+}
+
+export interface JobApiResponse {
+  page: number;
+  page_count: number;
+  items_per_page: number;
+  total: number;
+  results: Job[];
 }
