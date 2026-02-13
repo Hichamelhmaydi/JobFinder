ca31bcd94c253ed9245bb3ae7ab817f3b326cf01
Hichamelhmaydi <ct1594065@gmail.com>
2026-02-13 11:23:09 +0100

    update structure of components

--- src/app/features/home/home.component.ts
+++ src/app/features/home/home.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { JobComponent } from "../../shared/components/job-card/job-card.component";
+import { HeaderComponent } from "../../shared/components/layout/headre/headre.component";
+import { FooterComponent } from "../../shared/components/layout/footer/footer.component";
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, JobComponent, HeaderComponent, FooterComponent],
   templateUrl:'./home.component.html'
 })
 export class HomeComponent {}
---
+++ src/app/shared/components/job-card/job-card.component.ts
@@ -0,0 +1,28 @@
+import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { JobService } from '../../../core/services/job-api.service';
+import { Observable, catchError, of } from 'rxjs';
+import { JobApiResponse } from '../../../core/models/job.model';
+
+@Component({
+  selector: 'app-job',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './job-card.component.html',
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class JobComponent {
+
+  private jobService = inject(JobService);
+
+  jobs$: Observable<JobApiResponse> = this.jobService.getJobs({ page: 1 }).pipe(
+    catchError(() => of({
+      page: 1,
+      page_count: 0,
+      items_per_page: 0,
+      total: 0,
+      results: []
+    }))
+  );
+
+}
