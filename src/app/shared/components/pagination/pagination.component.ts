6ecc8b409db5e4aeeb5c3207da0f59956e66ca73
Hichamelhmaydi <ct1594065@gmail.com>
2026-02-13 11:39:38 +0100

    add pagination components

---
+++ src/app/shared/components/pagination/pagination.component.ts
@@ -0,0 +1,56 @@
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-pagination',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl:'./pagination.component.html'
+})
+export class PaginationComponent {
+  @Input() currentPage: number = 1;
+  @Input() totalPages: number = 1;
+  @Input() totalItems: number = 0;
+  @Output() pageChange = new EventEmitter<number>();
+
+  onPageChange(page: number | string): void {
+    if (typeof page === 'number' && page >= 1 && page <= this.totalPages && page !== this.currentPage) {
+      this.pageChange.emit(page);
+    }
+  }
+
+  getPageNumbers(): (number | string)[] {
+    const pages: (number | string)[] = [];
+    const maxVisible = 5;
+
+    if (this.totalPages <= maxVisible) {
+      for (let i = 1; i <= this.totalPages; i++) {
+        pages.push(i);
+      }
+    } else {
+      if (this.currentPage <= 3) {
+        for (let i = 1; i <= 4; i++) {
+          pages.push(i);
+        }
+        pages.push('...');
+        pages.push(this.totalPages);
+      } else if (this.currentPage >= this.totalPages - 2) {
+        pages.push(1);
+        pages.push('...');
+        for (let i = this.totalPages - 3; i <= this.totalPages; i++) {
+          pages.push(i);
+        }
+      } else {
+        pages.push(1);
+        pages.push('...');
+        pages.push(this.currentPage - 1);
+        pages.push(this.currentPage);
+        pages.push(this.currentPage + 1);
+        pages.push('...');
+        pages.push(this.totalPages);
+      }
+    }
+
+    return pages;
+  }
+}
